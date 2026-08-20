3d414c2222b1e9d562a97a397727c9b4a1c62558
cince <cince@gelirler.gov.tr>
2026-08-20 17:10:36 +0300

    v1.2.3

--- src/main/java/com/company/s3explorer/transfer/renderer/FileSizeRenderer.java
+++ src/main/java/com/company/s3explorer/transfer/renderer/FileSizeRenderer.java
@@ -4,9 +4,11 @@ import com.company.s3explorer.util.SizeFormatter;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
+import java.awt.*;
 
 public class FileSizeRenderer extends DefaultTableCellRenderer {
-
+    private int RIGHT_PADDING = 10;
+    
     public FileSizeRenderer() {
         setHorizontalAlignment(SwingConstants.RIGHT);
     }
@@ -14,5 +16,38 @@ public class FileSizeRenderer extends DefaultTableCellRenderer {
     @Override
     protected void setValue(Object value) {
         super.setValue(SizeFormatter.format((Long) value));
+    }
+
+    @Override
+    public Component getTableCellRendererComponent(
+            JTable table,
+            Object value,
+            boolean isSelected,
+            boolean hasFocus,
+            int row,
+            int column) {
+
+        Component component =
+                super.getTableCellRendererComponent(
+                        table,
+                        value,
+                        isSelected,
+                        hasFocus,
+                        row,
+                        column);
+
+        setHorizontalAlignment(
+                SwingConstants.RIGHT);
+
+        Insets insets = getInsets();
+
+        setBorder(
+                BorderFactory.createEmptyBorder(
+                        insets.top,
+                        insets.left,
+                        insets.bottom,
+                        RIGHT_PADDING));
+
+        return component;
     }
 }
