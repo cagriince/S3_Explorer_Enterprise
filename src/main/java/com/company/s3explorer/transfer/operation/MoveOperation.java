645a7a2c12c543c8e2ff95293b2aed741b3a1bed
cince <cince@gelirler.gov.tr>
2026-09-01 14:44:57 +0300

    v1.9.10 overwrite

--- src/main/java/com/company/s3explorer/transfer/operation/MoveOperation.java
+++ src/main/java/com/company/s3explorer/transfer/operation/MoveOperation.java
@@ -7,49 +7,110 @@ import com.company.s3explorer.transfer.model.TransferTask;
 public class MoveOperation extends AbstractTransferOperation {
 
     @Override
-    protected void doExecute(TransferRuntime runtime, TransferContext transferContext) throws Exception {
-        TransferTask task = runtime.getTask();
-        if (task.getRepositoryName().equals(task.getTargetRepositoryName())) {
-            updateProgressPercent(runtime, transferContext, 10);
+    protected void doExecute(
+            TransferRuntime runtime,
+            TransferContext transferContext) throws Exception {
+
+        TransferTask task =
+                runtime.getTask();
+
+        if (task.getRepositoryName()
+                .equals(task.getTargetRepositoryName())) {
+
+            updateProgressPercent(
+                    runtime,
+                    transferContext,
+                    10);
 
             try {
-                transferContext.getService(task.getRepositoryName()).copyObject(
-                        runtime.getTask().getBucket(),
-                        runtime.getTask().getObjectKey(),
-                        runtime.getTask().getTargetBucket(),
-                        runtime.getTask().getTargetObjectKey());
 
-                updateProgressPercent(runtime, transferContext, 70);
+                if (task.isOverwrite()) {
 
-                transferContext.getService(task.getRepositoryName()).deleteObject(
-                        runtime.getTask().getBucket(),
-                        runtime.getTask().getObjectKey());
+                    transferContext
+                            .getService(
+                                    task.getRepositoryName())
+                            .copyObjectOverwrite(
+                                    task.getBucket(),
+                                    task.getObjectKey(),
+                                    task.getTargetBucket(),
+                                    task.getTargetObjectKey());
+
+                } else {
+
+                    transferContext
+                            .getService(
+                                    task.getRepositoryName())
+                            .copyObject(
+                                    task.getBucket(),
+                                    task.getObjectKey(),
+                                    task.getTargetBucket(),
+                                    task.getTargetObjectKey());
+                }
+
+                updateProgressPercent(
+                        runtime,
+                        transferContext,
+                        70);
+
+                transferContext
+                        .getService(
+                                task.getRepositoryName())
+                        .deleteObject(
+                                task.getBucket(),
+                                task.getObjectKey());
+
             }
             finally {
-                updateProgressCompleted(runtime, transferContext);
+
+                updateProgressCompleted(
+                        runtime,
+                        transferContext);
             }
-        }
-        else {
+
+        } else {
+
             // Cross repository operation
-            updateProgressPercent(runtime, transferContext, 0);
+            updateProgressPercent(
+                    runtime,
+                    transferContext,
+                    0);
 
             try {
-                transferContext.getService(task.getRepositoryName()).copyObjectBetweenRepositories(
-                        runtime.getTask().getBucket(),
-                        runtime.getTask().getObjectKey(),
-                        transferContext.getService(runtime.getTask().getTargetRepositoryName()).getClient(),
-                        runtime.getTask().getTargetBucket(),
-                        runtime.getTask().getTargetObjectKey(),
-                        createProgressListener(runtime, transferContext));
 
-                updateProgressPercent(runtime, transferContext, 70);
+                transferContext
+                        .getService(
+                                task.getRepositoryName())
+                        .copyObjectBetweenRepositories(
+                                task.getBucket(),
+                                task.getObjectKey(),
+                                transferContext
+                                        .getService(
+                                                task.getTargetRepositoryName())
+                                        .getClient(),
+                                task.getTargetBucket(),
+                                task.getTargetObjectKey(),
+                                createProgressListener(
+                                        runtime,
+                                        transferContext));
 
-                transferContext.getService(task.getRepositoryName()).deleteObject(
-                        runtime.getTask().getBucket(),
-                        runtime.getTask().getObjectKey());
+                updateProgressPercent(
+                        runtime,
+                        transferContext,
+                        70);
+
+                transferContext
+                        .getService(
+                                task.getRepositoryName())
+                        .deleteObject(
+                                task.getBucket(),
+                                task.getObjectKey());
+
             }
             finally {
-                updateProgressCompleted(runtime, transferContext);
+
+                updateProgressCompleted(
+                        runtime,
+                        transferContext);
             }
         }
     }
