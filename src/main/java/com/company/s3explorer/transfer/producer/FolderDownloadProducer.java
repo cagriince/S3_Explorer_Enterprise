b4076459c7649d3fd5e8a3df7aed1d1f02ae4bf8
cince <cince@gelirler.gov.tr>
2026-09-11 10:00:44 +0300

    v1.12.6 encrypted file upload and download

--- src/main/java/com/company/s3explorer/transfer/producer/FolderDownloadProducer.java
+++ src/main/java/com/company/s3explorer/transfer/producer/FolderDownloadProducer.java
@@ -1,5 +1,6 @@
 package com.company.s3explorer.transfer.producer;
 
+import com.company.s3explorer.security.EncryptionConfig;
 import com.company.s3explorer.transfer.context.TransferContext;
 import com.company.s3explorer.transfer.model.TransferTask;
 import com.company.s3explorer.transfer.queue.TransferQueue;
@@ -11,6 +12,7 @@ public class FolderDownloadProducer
         extends AbstractFolderTransferProducer {
 
     private final Path localFolder;
+    private final EncryptionConfig encryptionConfig;
 
     public FolderDownloadProducer(
             TransferContext context,
@@ -20,6 +22,25 @@ public class FolderDownloadProducer
             String prefix,
             Path localFolder) {
 
+        this(
+                context,
+                queue,
+                repository,
+                bucket,
+                prefix,
+                localFolder,
+                null);
+    }
+
+    public FolderDownloadProducer(
+            TransferContext context,
+            TransferQueue queue,
+            String repository,
+            String bucket,
+            String prefix,
+            Path localFolder,
+            EncryptionConfig encryptionConfig) {
+
         super(
                 context,
                 queue,
@@ -28,6 +49,7 @@ public class FolderDownloadProducer
                 prefix);
 
         this.localFolder = localFolder;
+        this.encryptionConfig = encryptionConfig;
     }
 
     @Override
@@ -51,6 +73,7 @@ public class FolderDownloadProducer
                 .objectKey(object.key())
                 .localPath(target)
                 .size(object.size())
+                .encryptionConfig(encryptionConfig)
                 .affectsObjectList(false)
                 .affectsFolderTree(false)
                 .group(group)
