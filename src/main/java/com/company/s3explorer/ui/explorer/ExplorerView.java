03afb98428bb92b9a74cca22d6ba655d651575aa
cince <cince@gelirler.gov.tr>
2026-09-12 00:18:08 +0300

    v1.16 Popup Menu Icons

--- src/main/java/com/company/s3explorer/ui/explorer/ExplorerView.java
+++ src/main/java/com/company/s3explorer/ui/explorer/ExplorerView.java
@@ -450,6 +450,7 @@ public final class ExplorerView {
 
     private void createPopupMenu() {
         filePopup = new JPopupMenu();
+
         JMenuItem createFolderMenu = new JMenuItem(newFolderAction);
         JMenuItem uploadMenu = new JMenuItem(uploadAction);
         JMenuItem uploadEncryptedMenu = new JMenuItem(uploadEncryptedAction);
@@ -462,6 +463,18 @@ public final class ExplorerView {
         JMenuItem cutMenu = new JMenuItem(cutAction);
         JMenuItem pasteMenu = new JMenuItem(pasteAction);
 
+        setMenuItemIcon(createFolderMenu, newFolderAction);
+        setMenuItemIcon(uploadMenu, uploadAction);
+        setMenuItemIcon(uploadEncryptedMenu, uploadEncryptedAction);
+        setMenuItemIcon(downloadMenu, downloadAction);
+        setMenuItemIcon(downloadDecryptedMenu, downloadDecryptedAction);
+        setMenuItemIcon(deleteMenu, deleteAction);
+        setMenuItemIcon(renameMenu, renameAction);
+        setMenuItemIcon(propertiesMenu, propertiesAction);
+        setMenuItemIcon(copyMenu, copyAction);
+        setMenuItemIcon(cutMenu, cutAction);
+        setMenuItemIcon(pasteMenu, pasteAction);
+
         filePopup.add(createFolderMenu);
         filePopup.add(deleteMenu);
         filePopup.add(renameMenu);
@@ -473,10 +486,11 @@ public final class ExplorerView {
         filePopup.add(downloadMenu);
         filePopup.add(downloadDecryptedMenu);
         filePopup.addSeparator();
-        
+
         filePopup.add(copyMenu);
         filePopup.add(cutMenu);
         filePopup.add(pasteMenu);
+
         filePopup.addPopupMenuListener(new PopupMenuListener() {
             @Override
             public void popupMenuWillBecomeVisible(PopupMenuEvent e) {
@@ -485,8 +499,10 @@ public final class ExplorerView {
 
                 downloadDecryptedMenu.setVisible(
                         isEncryptionConfigured());
-                
-                pasteMenu.setEnabled(clipboardEmpty == null || !clipboardEmpty.getAsBoolean());
+
+                pasteMenu.setEnabled(
+                        clipboardEmpty == null
+                                || !clipboardEmpty.getAsBoolean());
             }
 
             @Override
@@ -497,6 +513,19 @@ public final class ExplorerView {
             public void popupMenuCanceled(PopupMenuEvent e) {
             }
         });
+    }
+
+    private void setMenuItemIcon(
+            JMenuItem menuItem,
+            Action action) {
+
+        Icon icon =
+                (Icon) action.getValue(
+                        Action.LARGE_ICON_KEY);
+
+        if (icon != null) {
+            menuItem.setIcon(icon);
+        }
     }
 
     private boolean isEncryptionConfigured() {
