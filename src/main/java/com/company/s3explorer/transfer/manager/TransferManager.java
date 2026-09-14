671fbf322f4ad1fe6379e944444137fcb8e11bed
cince <cince@gelirler.gov.tr>
2026-09-14 14:14:10 +0300

    v1.23 Bugs, selecting processes

--- src/main/java/com/company/s3explorer/transfer/manager/TransferManager.java
+++ src/main/java/com/company/s3explorer/transfer/manager/TransferManager.java
@@ -71,7 +71,11 @@ public class TransferManager {
     }
 
     public boolean cancelGroup(TransferGroup group) {
-
+        System.out.println(
+                "[CANCEL GROUP] requested group=" +
+                        group.getDisplayName() +
+                        " id=" +
+                        group.getId());
         if (group == null
                 || group.getId() == null) {
 
@@ -102,7 +106,11 @@ public class TransferManager {
                 groupProducers.remove(groupId);
 
         if (producerRuntime != null) {
-
+            System.out.println(
+                    "[CANCEL GROUP] requested group=" +
+                            group.getDisplayName() +
+                            " id=" +
+                            group.getId());
             boolean producerCancelled =
                     producerExecutor.cancel(
                             producerRuntime);
@@ -110,6 +118,13 @@ public class TransferManager {
             cancelled =
                     cancelled
                             || producerCancelled;
+        }
+        else {
+            System.out.println(
+                    "[CANCEL GROUP] NO PRODUCER FOUND group=" +
+                            group.getDisplayName() +
+                            " id=" +
+                            group.getId());
         }
 
         return cancelled;
